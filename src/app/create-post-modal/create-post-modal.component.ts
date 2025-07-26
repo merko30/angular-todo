3538285286d6e556069245b120421f11d5764e2e
Merim <merim.hasanbegovic@outlook.com>
2025-07-26 16:14:40 +0200

    feat: modal

---
+++ src/app/create-post-modal/create-post-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post-modal',
+  imports: [],
+  templateUrl: './create-post-modal.component.html',
+  styleUrl: './create-post-modal.component.css'
+})
+export class CreatePostModalComponent {
+
+}
